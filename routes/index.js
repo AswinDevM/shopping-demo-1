082d68a3f01ead3a6cf5fe0080fbb08e1fc2423b
AswinDevM <aswindevm143@gmail.com>
2024-05-11 10:49:38 +0530

    all files changed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Welcome to the index page');
 });
 
 module.exports = router;
+
+
+
+
+
+
+/*
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, '')));
+
+// Define route to serve HTML file
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server running on port ' + port);
+});*/
